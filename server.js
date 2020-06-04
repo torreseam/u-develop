c0539843158558630f01c4867b90944bdcecc630
torreseam <615694479+torreseam@users_noreply.github.com>
2020-06-03 22:50:45 -0500

    add post route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Dependancies 
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 //Port
 const PORT = process.env.PORT || 3001;
@@ -70,6 +71,36 @@ app.delete('/api/candidate/:id', (req, res) => {
         });
     });
 });
+
+// Create-add candidate using POST-name and id need to change as it cannot be reused to protect from duplicates 
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    //allows to add candidates in insomnia
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
+//
+
+
 
 
 // CATCHALL Route for no response to server---Default response for any other request(Not Found) Catch all
